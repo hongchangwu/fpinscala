ba48b072dfd89679a35177b74b00b1ab5c7cb34e
Hongchang Wu <wuhc85@gmail.com>
2014-12-18 00:02:21 -0500

    New Prop

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -8,21 +8,68 @@ import Gen._
 import Prop._
 import java.util.concurrent.{Executors,ExecutorService}
 
+//import fpinscala.testing.Prop.{FailedCase, SuccessCount}
+
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
+trait Prop1 {
   def check: Boolean
 
-  def &&(p: Prop): Prop = new Prop {
+  def &&(p: Prop1): Prop1 = new Prop1 {
     override def check = this.check || p.check
   }
 }
 
+case class Prop(run: (TestCases, RNG) => Result) {
+  def &&(p: Prop): Prop = Prop {
+    (n, rng) => run(n, rng) match {
+      case Passed => p.run(n, rng)
+      case f: Falsified => f
+    }
+  }
+
+  def ||(p: Prop): Prop = Prop {
+    (n, rng) => run(n, rng) match {
+      case Passed => Passed
+      case _ => p.run(n, rng)
+    }
+  }
+}
+
 object Prop {
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  type FailedCase = String
+  type SuccessCount = Int
+  type TestCases = Int
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+  case object Passed extends Result {
+    override def isFalsified = false
+  }
+  case class Falsified(failure: FailedCase, successes: SuccessCount)
+    extends Result {
+    override def isFalsified = true
+  }
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
 }
 
 /*object Gen {
